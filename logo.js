4a19e47afa121ae954eed9128c99a2d2a062ae68
Trevor Hunt <157053920+huntt3@users.noreply.github.com>
2025-06-13 23:41:02 +0000

    Commit 6. Finalize charity: water fade in and out.

--- logo.js
+++ logo.js
@@ -1,4 +1,12 @@
-setTimeout(() => {
-        const element = document.getElementById("logo-fade-img");
-        element.remove();
-      }, 4000);
+window.addEventListener('DOMContentLoaded', () => {
+        const giantBox = document.getElementById('giant-box');
+        if (giantBox) {
+          setTimeout(() => {
+            giantBox.remove();
+          }, 4000);
+        }
+        setTimeout(() => {
+          const element = document.getElementById("logo-fade-img");
+          element.remove();
+        }, 4000);
+      });
